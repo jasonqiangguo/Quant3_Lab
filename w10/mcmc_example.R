5257c437207e170d66d98204967038b4f7eea0af
jasonqiangguo <jasonguo0509@gmail.com>
2016-11-18 17:45:01 -0500

    quant3_lab

--- w10/mcmc_example.R
+++ w10/mcmc_example.R
@@ -4,6 +4,46 @@
 ## Quant III Lab 10
 #########################################################
 
+################
+# markov chain #
+################
+
+#Every person in a society can be either sick (S), healthy but not immune (H), or immune
+#and healthy (I). People move from one state to another according to the following Markov
+#transition probabilities:
+
+transition <- matrix(c(0.9,0,0.1,0.6,0.4,0,0,0,1),nrow=3,byrow=TRUE)
+
+# Plot the distribution of the states as a function of n, starting with a state where 50% of
+# people are healthy and 50% are sick.
+
+p <- as.vector(c(0, 0.5,0.5))
+
+x <- rep(NA, 100)
+y <- rep(NA, 100)
+z <- rep(NA, 100)
+
+x[1] <- 0
+y[1] <- 0.5
+z[1] <- 0.5
+
+for (i in 1:99){ 
+  d <- t(p) %*% transition # distribution at t = i + 1, using transition matrix at t = i
+  x[i+1] <- d[1]
+  y[i+1] <- d[2]
+  z[i+1] <- d[3]
+  transition <- transition %*% transition # transition matrix at t = i + 1
+}
+
+#distribution <- cbind(x,y,z)
+#plot.ts(distribution)
+
+plot(x, type="l", col=1, ylim=c(0,1), lwd=2,
+     ylab="Probability", main="Distribution Across Time",xlab="Time")
+lines(y, col=2,lwd=2 ,lty=2)
+lines(z, col=4, lwd=2)
+legend("right", legend=c("Sick","Healthy","Immune"), col=c(1,2,4), lty=c(1,2,1), lwd=2)
+
 ##################
 ## Gibbs Sampler #
 ##################
